814e87adadac872633d114d1c644a17d9c0de3b6
park <hnc21@naver.com>
2020-07-09 14:32:21 +0900

    앱 기본 동작 완료 .

--- APP/app/src/main/java/com/sundaypark/factory/ndcl/ui/HomeFragment.kt
+++ APP/app/src/main/java/com/sundaypark/factory/ndcl/ui/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.sundaypark.factory.ndcl.ui
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
+import androidx.core.animation.addListener
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -19,6 +21,7 @@ import com.sundaypark.factory.ndcl.domain.CODE_FAILURE
 import com.sundaypark.factory.ndcl.domain.CODE_SERVERERROR
 import com.sundaypark.factory.ndcl.ui.viewmodel.HomeViewModel
 import com.sundaypark.factory.ndcl.utils.SharedPreferencesManager
+import android.animation.AnimatorListenerAdapter as AnimatorListenerAdapter1
 
 // 위치 정보 확인
 // 버전확인
@@ -47,23 +50,7 @@ class HomeFragment : Fragment() {
             false
         ).apply {
 
-            Viewmodel.Version.observe(viewLifecycleOwner, Observer {
-                if (SharedPreferencesManager.getVersionDate() != it.date.time) {
-                    // 지역 업데이트 필요
-                    Log.i(TAG, "주소 업데이트 필요 ")
-                    BindingView.textHome.text = "주소 업데이트 중 "
-                    val Version = it
-                    Viewmodel.getCitys.observe(viewLifecycleOwner, Observer {
-                        BindingView.textHome.text = "주소 업데이트 완료"
-                        SharedPreferencesManager.setVersionDate(Version)// 저장
-                        gotoListPage()
-                    })
-                } else {
-                    BindingView.textHome.text = "주소 DB 변동 없음 "
-                    gotoListPage()
 
-                }
-            })
             Viewmodel.onError.observe(viewLifecycleOwner, Observer {
                 var text = ""
                 when (it.code) {
@@ -85,8 +72,29 @@ class HomeFragment : Fragment() {
             })
 
         }
+        ObjectAnimator.ofFloat(BindingView.logo ,"translationY" , 1000f , 0f).apply {
+            duration = 2000
+            start()
+            addListener(onEnd = {
+                Viewmodel.Version.observe(viewLifecycleOwner, Observer {
+                    if (SharedPreferencesManager.getVersionDate() != it.date.time) {
+                        // 지역 업데이트 필요
+                        Log.i(TAG, "주소 업데이트 필요 ")
+                        BindingView.textHome.text = "주소 업데이트 중 "
+                        val Version = it
+                        Viewmodel.getCitys.observe(viewLifecycleOwner, Observer {
+                            BindingView.textHome.text = "주소 업데이트 완료"
+                            SharedPreferencesManager.setVersionDate(Version)// 저장
+                            gotoListPage()
+                        })
+                    } else {
+                        BindingView.textHome.text = "주소 DB 변동 없음 "
+                        gotoListPage()
 
-        BindingView.homeData = Viewmodel
+                    }
+                })
+            })
+        }
 
         return BindingView.root
     }
